c771d7e5d2a5383c47e3cdc4ca352780c535f473
olenaZaruba <z_len4ik@yahoo.fr>
2023-08-20 21:09:30 +0200

    task-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const photo = document.querySelector(".gallery__image");
+
 const cardsContainer = document.querySelector(".gallery");
 const cardsMarkup = createGalleryMarkup(galleryItems);
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const cardsContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryMarkup(galleryItems);
+
+cardsContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryMarkup(cards) {
+  return cards
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+     
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
